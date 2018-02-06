0f64fb78156d4c454886b5d49941e64b19590b97
joangonzalvo <joangocap@gmail.com>
2018-02-06 15:51:57 +0100

    Afegits funcions query, bind i execute

--- index.php
+++ index.php
@@ -1,4 +1,22 @@
 <?php
 
-    echo "Funciona";
-
+    require 'sys/DB.php';
+    
+    define('CONF',__DIR__.'/config.json');
+    
+    
+    
+    use App\Sys\DB;
+    
+    //connect
+    $gbd =DB::getInstance();
+    //consult
+    $sql='INSERT INTO users VALUES ("userbeans", "123pass", "01-01-17", "beans@mail.com")'; 
+    
+    //set param
+    $gdb->bind();
+    //query
+    $gdb->query($sql);
+    //execute
+    $gdb->execute();
+    
